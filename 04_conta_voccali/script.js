5437d45bb1d3695d64568a873a7c09ebee7aab61
lucagrandieri <grandieriluca@gmail.com>
2026-02-10 18:17:53 +0100

    creato una funzione che restituisce il numero di vocali contenute in una parola

---
+++ 04_conta_voccali/script.js
@@ -0,0 +1,20 @@
+/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+const word = 'javascript';
+
+// Dichiara la funzione qui.
+function vowelsCounter(word1) {
+    let vowels = 0;
+    for (let i = 0; i < word1.length; i++) {
+    if (word1[i] === 'a' || word1[i] === 'e' || word1[i] === 'i' || word1[i] === 'o' || word1[i] === 'u') {
+     vowels += 1
+    }
+}
+    return vowels;
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+vowelsCounter(word)
+console.log(vowelsCounter(word));
+
+
+//Risultato atteso se si passa 'javascript': 3 (a, a, i)
