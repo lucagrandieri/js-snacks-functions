181cdbc7e7b5c1292e9513ebc51cd02b1277b98e
lucagrandieri <grandieriluca@gmail.com>
2026-02-10 17:05:05 +0100

    corretto esercizio (avevo letto male la traccia)

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,29 +1,31 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-let namesL = []
 
 // Dichiara la funzione qui.
-function returnInitial(arr) { // function invocation
+function returnInitial(arr, letter) { // function invocation
+    let result = [];
     for (let i = 0; i < arr.length; i++) {
-    let initial = arr[i].charAt(0)
-    if (initial === 'L') {
-        namesL.push(arr[i])
+        if (arr[i].charAt(0) === letter) {
+            result.push(arr[i]);
+        }
     }
-}
-}
-
-const returnInitial = arr => { // arrow function
-    for (let i = 0; i < arr.length; i++) {
-    let initial = arr[i].charAt(0)
-    if (initial === 'L') {
-        namesL.push(arr[i])
-    }
-}
+    return result;
 } 
 
+const returnInitial = (arr, letter) => { // arrow function
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].charAt(0) === letter) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+
 // Invoca la funzione qui e stampa il risultato in console
-returnInitial(names)
-console.log(namesL);
+returnInitial(names, 'L')
+console.log(returnInitial(names, 'L'));
+
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
